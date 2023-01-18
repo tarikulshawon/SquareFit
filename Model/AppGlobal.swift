0dc9ae29872b5c56f3447a83edfb191ac5c0a9b6
ti_shawon <1405050.ti@ugrad.cse.buet.ac.bd>
2023-01-18 21:47:59 +0600

    Revert "Add files via upload"
    This reverts commit ab6913b64b6e484a9e18a3ff9ed8a3005db0a685.

--- Model/AppGlobal.swift
+++ Model/AppGlobal.swift
@@ -19,8 +19,6 @@ var titleColor = UIColor(red: 122.0/255.0, green: 48.0/255.0, blue: 231.0/255.0,
 let overLayHeight = 300
 let canVasHeight = 280
 let canvasWidth:CGFloat = 50
-let shapeVcHeight = 350
-let framesVcHeight = 300
 
 
 
