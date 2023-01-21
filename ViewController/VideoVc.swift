50914069a86e01b0daf06490b4e7b8cdee097e15
sadiqulamin <113623819+sadiqulamin@users.noreply.github.com>
2023-01-21 11:24:50 +0600

    ..
    video added

--- ViewController/VideoVc.swift
+++ ViewController/VideoVc.swift
@@ -7,8 +7,54 @@
 
 import UIKit
 
-class VideoVc: UIViewController, UIGestureRecognizerDelegate {
+class VideoVc: UIViewController, UIGestureRecognizerDelegate, allDelegate {
     
+    func sendCanvasData(width: Int, height: Int) {
+         
+        let result = CGFloat(height*Int(previewContainer.frame.width))/CGFloat(width)
+        UIView.animate(withDuration: 0.4, animations: {
+            
+            if result > self.previewContainer.frame.height {
+                let result1 = CGFloat(width*Int(self.previewContainer.frame.height))/CGFloat(height)
+                self.widthForV.constant = result1
+                self.heightForV.constant =  self.previewContainer.frame.height
+                
+            } else {
+                
+                self.widthForV.constant = self.previewContainer.frame.width
+                self.heightForV.constant =  result
+            }
+            
+            self.view.layoutIfNeeded()
+             
+        }) { _ in
+            
+        }
+        
+    }
+    
+    
+    
+    func stickerData(sticker: String) {
+         
+    }
+    
+    func sendFrame(frames: String) {
+         
+    }
+    
+    func sendOverLay(image: UIImage?) {
+         
+    }
+    
+    func sendAdjust(value: Float, index: Int) {
+         
+    }
+    
+    
+    @IBOutlet weak var btnCollectionView: UICollectionView!
+    @IBOutlet weak var widthForV: NSLayoutConstraint!
+    @IBOutlet weak var heightForV: NSLayoutConstraint!
     
     var panRecogniser:UIPanGestureRecognizer! = nil
     var pinchRecogniser:UIPinchGestureRecognizer! = nil
@@ -22,9 +68,9 @@ class VideoVc: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var previewContainer: UIView!
     
     
-     let  flipScaleY = 1.0
+    let  flipScaleY = 1.0
     let flipScaleX = 1.0
-     var scale = 1.0
+    var scale = 1.0
  
     var videoDuration:CGFloat = 1.0
     var playerItem:AVPlayerItem! = nil
@@ -37,7 +83,10 @@ class VideoVc: UIViewController, UIGestureRecognizerDelegate {
     let isMute = false
     let currentProgress:CGFloat = 0.0
     let selectedColor = UIColor.red
+    var cellWidth:CGFloat = 60
+    var cellGap:CGFloat =  0
     @IBOutlet weak var backgroundImgView: UIImageView!
+    var btnArray: NSArray! = nil
 
     
     
@@ -50,8 +99,44 @@ class VideoVc: UIViewController, UIGestureRecognizerDelegate {
         self.initalGesture()
         self.initVideoAsset()
         
+        let path = Bundle.main.path(forResource: "videobtn", ofType: "plist")
+        btnArray = NSArray(contentsOfFile: path!)
+        initCollectionView()
+        self.perform(#selector(self.updateFrame), with: self, afterDelay:0.1)
+        
         // Do any additional setup after loading the view.
     }
+    
+    func initCollectionView() {
+        let emptyAutomationsCell = RatioCell.nib
+        btnCollectionView?.register(emptyAutomationsCell, forCellWithReuseIdentifier: RatioCell.reusableID)
+        btnCollectionView.delegate = self
+        btnCollectionView.dataSource = self
+    }
+    
+    
+    @objc func updateFrame() {
+        
+        widthForV.constant = previewContainer.frame.width
+        heightForV.constant = previewContainer.frame.width
+        
+        
+        let totalCellWidth = cellWidth * CGFloat(btnArray.count)
+        let totalSpacingWidth = cellGap * CGFloat((btnArray.count - 1))
+        
+        let leftInset = (btnCollectionView.frame.size.width - CGFloat(totalCellWidth + totalSpacingWidth)) / 2
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        layout.minimumInteritemSpacing = cellGap
+        layout.minimumLineSpacing = cellGap
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        btnCollectionView?.collectionViewLayout = layout
+        
+        layout.scrollDirection = .horizontal
+        btnCollectionView?.reloadData()
+    }
+    
     
     func initVideoAsset() {
         
@@ -186,4 +271,130 @@ class VideoVc: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
+}
+
+extension VideoVc:UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RatioCell.reusableID,
+            for: indexPath) as? RatioCell else {
+            return RatioCell()
+        }
+        
+        
+        cell.iconImv.contentMode = .scaleAspectFit
+        cell.iconLabel.textColor = unselectedColor
+        cell.heightForLabel.constant = 20.0
+        cell.backgroundColor = UIColor.clear
+        
+        if let value = btnArray[indexPath.row] as? String  {
+            
+            cell.iconImv.image = UIImage(named: value)?.withRenderingMode(.alwaysTemplate)
+            cell.iconImv.tintColor = UIColor.white
+            cell.iconLabel.textColor = unselectedColor
+            cell.iconLabel.text = value
+        }
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 60, height: 60)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int { 1 }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return btnArray.count
+    }
+    
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
+        let cell = collectionView.cellForItem(at: indexPath) as? RatioCell
+        
+        if let titleName = cell?.iconLabel.text {
+            
+            
+            let vc = CustomModalViewController()
+            
+            vc.delegateForEditedView = self
+            vc.canVas.isHidden = true
+            vc.stickerVc.isHidden = true
+            vc.overlayVc.isHidden = true
+            vc.shapeVc.isHidden = true
+            vc.frameVc.isHidden = true
+            vc.adjustVc.isHidden = true
+            vc.filterVc.isHidden = true
+            
+            if titleName.contains("Adjust") {
+                vc.defaultHeight = CGFloat(adjustHeight)
+                vc.maximumContainerHeight = CGFloat(adjustHeight)
+                vc.adjustVc.isHidden = false
+            }
+            
+            if titleName.contains("Frames") {
+                vc.defaultHeight = CGFloat(framesVcHeight)
+                vc.frameVc.isHidden = false
+            }
+            
+            if titleName.contains("Canvas") {
+                vc.defaultHeight = CGFloat(canVasHeight)
+                vc.maximumContainerHeight  = CGFloat(canVasHeight)
+                vc.canVas.isHidden = false
+            }
+            
+            else if titleName.contains("Overlay") {
+                vc.defaultHeight = CGFloat(overLayHeight)
+               // vc.maximumContainerHeight = CGFloat(overLayHeight)
+                vc.overlayVc.isHidden = false
+                
+               
+            }
+            else if titleName.contains("Shape") {
+                vc.defaultHeight = CGFloat(shapeVcHeight)
+                vc.maximumContainerHeight = CGFloat(shapeVcHeight)
+                vc.shapeVc.isHidden = false
+                
+            }
+            else if titleName.contains("Filter") {
+                vc.defaultHeight = CGFloat(filterHeight)
+                vc.filterVc.isHidden = false
+                
+            }
+            else {
+                vc.defaultHeight = 300
+                vc.stickerVc.isHidden = false
+
+            }
+            vc.typeName = titleName
+            
+            presentSheetViewController(with: vc, initialHeight: vc.defaultHeight, maxHeight: vc.maximumContainerHeight)
+        }
+    }
+    
+}
+
+private extension VideoVc {
+    func presentSheetViewController(with vc: UIViewController, initialHeight: CGFloat, maxHeight: CGFloat) {
+        if let sheet = vc.sheetPresentationController {
+            sheet.detents = [
+                .custom(identifier: .init("\(initialHeight)") ,resolver: { _ in initialHeight }),
+                .medium(),
+                .custom(identifier: .init("\(maxHeight)") ,resolver: { _ in maxHeight })
+            ]
+            
+            sheet.selectedDetentIdentifier = .none
+            sheet.largestUndimmedDetentIdentifier = .medium
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+            sheet.prefersEdgeAttachedInCompactHeight = true
+            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+        }
+        
+        present(vc, animated: true, completion: nil)
+    }
 }
